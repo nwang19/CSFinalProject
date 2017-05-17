e9d4e6fcd7f8daab37ed9ae159a4b451f69b3172
RiversMountains <vuppalad@gmail.com>
2017-05-17 10:21:57 -0700

    Update Player.java

--- CSFinalProject/src/Player.java
+++ CSFinalProject/src/Player.java
@@ -30,29 +30,6 @@ public class Player
 		lives = numLives;
 	}
 
-	//passes location to Controller to check if it can move there by calling Grid’s checkNextLoc
-	public Location checkLocation()
-	{
-		if(upKey)
-	        {
-	            return new Location(loc.getRow(), loc.getCol()+1);
-	        }
-	      else if(leftKey)
-	        {
-	            return new Location(loc.getRow()-1, loc.getCol());
-	        }
-	      else if(rightKey)
-	        {
-	            return new Location(loc.getRow()+1, loc.getCol());
-	        }
-	      else if(rightKey && upKey)
-	        {
-	            return new Location(loc.getRow()+1, loc.getCol()+1);
-	        }
-	        return null; 
-		
-	}
-
 	public void setLocation(Location newLoc)
 	{
 		loc = newLoc;
