7a6d765c9c73141af28d453e00977c7e4f074b7c
nwang19 <wangnicole19@gmail.com>
2017-05-18 11:18:46 -0700

    Update Controller.java

--- CSFinalProject/src/Controller.java
+++ CSFinalProject/src/Controller.java
@@ -9,26 +9,30 @@ public class Controller implements KeyListener, ActionListener
 	private GUI gui;
 	private Player player;
 	private Timer timer;
+	private Timer jumpTimer;
 	private int levelNum;
 	private Level level;
+	private int counter;
 
 	public Controller() 
 	{
 		timer = new Timer(250, this);
+		jumpTimer = new Timer(250, this);
 		player = new Player();
 		levelNum++;
 		level = new Level(levelNum);
+		counter = 0;
 	}
 
 	// constructs Grid with correct set of obstacles and platforms, and update’s
 	// GUI’s display
 	public void initialize() 
 	{
-		// add stuff to obstacles and platforms
 		gui = new GUI();
 		area = new Grid(level.getObstacles(), level.getPlatforms(), level.getEnd());
 	}
 
+	//checks if either right or left arrow key is pressed
 	public void keyPressed(KeyEvent event) 
 	{
 		if (event.getKeyCode() == KeyEvent.VK_RIGHT) 
@@ -39,21 +43,25 @@ public class Controller implements KeyListener, ActionListener
 		timer.addActionListener(this);
 	}
 	
+	//checks if user hit the up arrow key. If yes, starts timer and sets player's jumping state to true
 	public void keyTyped(KeyEvent event)
 	{
 		if (event.getKeyCode() == KeyEvent.VK_UP) 
 		{
-			player.setState(Player.UP);
-			processUp();
+			jumpTimer.start();
+			jumpTimer.addActionListener(this);
+			player.setYState(true);
 		}
 	}
 
+	//Stops the player's movement and resets player's state to still
 	public void keyReleased(KeyEvent event) 
 	{
 		timer.stop();
 		player.setState(Player.STILL);
 	}
 
+	//Moves player to next location or beginning of game (if player hit an obstacle). Calls GUI to update display of position
 	public void processMove(Point nextLoc) 
 	{
 		if(nextLoc != null)
@@ -94,41 +102,61 @@ public class Controller implements KeyListener, ActionListener
 				player.setLocation(left);
 			}
 		 gui.updateScreen(player, obstacles, platforms);
-
 		}
 	} */
 
-	public void processJump(Point moveLoc)
+	//returns new location player will move to when jumping
+	public Point processJump(Point moveLoc)
 	{
-		if (counter > 4)
+		if (player != null)
 		{
-			player.setYState(false);
-			counter = 0;
+			if (counter > 4)
+			{
+				jumpTimer.stop();
+				player.setYState(false);
+				counter = 0;
+				return moveLoc;
+			}
+			else
+			{
+				counter++;
+				if (counter == 1)
+					return new Point((int)moveLoc.getX(), (int)(moveLoc.getY() + 20));
+				else if (counter == 2)
+					return new Point((int)moveLoc.getX(), (int)(moveLoc.getY() + 10));
+				else if (counter == 3)
+					return new Point((int)moveLoc.getX(), (int)(moveLoc.getY() - 10));
+				else
+					return new Point((int)moveLoc.getX(), (int)(moveLoc.getY() - 20));
+			}
 		}
-		//else... all new move locs accounting gravity for counter = 0-4 
+		return null;
 	}
 
 	//moves player back to beginning of level and decrements lives
 	public void resetLevel() 
 	{
 		player.setLocation(Level.start);
-		player.setLives(player.getLives() - 1);
-		gui.updateLifeImg(player.getLives());
+		//player.setLives(player.getLives() - 1);
+		//gui.updateLifeImg(player.getLives());
 	}
 
+	//when timer goes off, calls player move method
 	public void actionPerformed(ActionEvent e)
 	{
-		Point loc = player.getLocation();
-		Point moveLoc = loc;
-		if (player.getState() == Player.LEFT)
-			moveLoc = new Point((int)loc.getX()-10, (int)loc.getY());
-		else if (player.getState() == Player.RIGHT)
-			moveLoc = new Point((int)loc.getX()+10, (int)loc.getY());
-		if (player.getYstate() == true)
-			moveLoc = processJump(moveLoc);
+		Point moveLoc = player.getLocation();
+		if (e.getSource() == timer)
+		{
+			if (player.getState() == Player.LEFT)
+				moveLoc = new Point((int)moveLoc.getX()-10, (int)moveLoc.getY());
+			else if (player.getState() == Player.RIGHT)
+				moveLoc = new Point((int)moveLoc.getX()+10, (int)moveLoc.getY());
+		}
+		else if (e.getSource() == jumpTimer)
+		{
+			if (player.getYState() == true)
+				moveLoc = processJump(moveLoc);
+		}
 		processMove(area.checkNextLoc(moveLoc, levelNum, player.getState(), player.getYState()));
 	}
-
-	
-
 }
