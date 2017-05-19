7d0aeb892c90773ba2dd01b6b268c90003d1e249
nwang19 <wangnicole19@gmail.com>
2017-05-19 01:13:01 -0700

    "Lives" piece removed, has Image field
    but idk how to load images?? h e l p

--- CSFinalProject/src/Player.java
+++ CSFinalProject/src/Player.java
@@ -1,21 +1,25 @@
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;   
 
 public class Player extends Rectangle
 {
 	//Job: stores lives, gets location to move to next, and updates its record of its position.
 
-	private int lives; 
-	private Point loc;
+	//private int lives; 
 	private int state;
+	private boolean yState;
+	private Image person;
 	public static final int STILL = 0;
 	public static final int LEFT = 1;
 	public static final int RIGHT = 2;
-	private boolean yState;
 	
 	public Player()
 	{
-		lives = 3;
+		//lives = 3;
 		state = 0;
 		yState = false;
 	}
@@ -23,12 +27,16 @@ public class Player extends Rectangle
 	public Player(int x, int y, int width, int height)
 	{
 		super(x, y, width, height);
-		lives = 3;
+		//lives = 3;
+		PictLoader test = new PictLoader();// when using IDE
+		//JarPictLoader test = new JarPictLoader();// when using JAR file
+		ImageIcon personIcon = test.getperson();
+		person = personIcon.getImage();
 		state = 0;
 		yState = false;
 	}
 
-	public int getLives()
+	/*public int getLives()
 	{
 		return lives;
 	}
@@ -36,7 +44,7 @@ public class Player extends Rectangle
 	public void setLives(int numLives)
 	{
 		lives = numLives;
-	}
+	}*/
 
 	public void setState(int status)
 	{
