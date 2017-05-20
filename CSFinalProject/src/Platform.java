84db9028ccd07b08defefc9534824f5e117cfd84
RiversMountains <vuppalad@gmail.com>
2017-05-19 19:59:46 -0700

    Update Platform.java
    platform-with image

--- CSFinalProject/src/Platform.java
+++ CSFinalProject/src/Platform.java
@@ -1,6 +1,8 @@
 import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import java.awt.Rectangle;
+import java.net.URL;
 
 public class Platform extends Rectangle
 {
@@ -12,18 +14,23 @@ public class Platform extends Rectangle
 	public Platform(int x, int y, int w, int h)
 	{
 		super(x, y, width, height);
+		
+		
 	}
 	
 	public Image getImg()
 	{
+		ClassLoader cldr = this.getClass().getClassLoader();	
+		URL platform   = cldr.getResource("platform.png");	
+		ImageIcon blockIcon = new ImageIcon(platform);
+		blockImg = blockIcon.getImage();
 		return blockImg;
 	}
 	
 	//how do we add images?? can be do something like this, or do we have to load it into the gui?
 	public void setImg(String imgName)
 	{
-		ImageIcon blockIcon = new ImageIcon(imgName);
-		blockImg = blockIcon.getImage();
+		
 		//int load = fishIcon.getImageLoadStatus(); System.out.println("fish load " + load);
 		//System.out.println("fish load " + load);
 	}
