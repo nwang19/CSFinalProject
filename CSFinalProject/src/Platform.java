da532eec48718534ee946dc7b4a13c1d800a987a
nwang19 <wangnicole19@gmail.com>
2017-05-23 02:49:47 -0700

    Update Platform.java

--- CSFinalProject/src/Platform.java
+++ CSFinalProject/src/Platform.java
@@ -1,16 +1,12 @@
-import java.awt.Image;
-import javax.swing.ImageIcon;
 import java.awt.Rectangle;
 
 public class Platform extends Rectangle
 {
-
-	private Image blockImg;
 	static int width = 300;
-	static int height =200;
+	static int height = 20;
 	
 	public Platform(int x, int y, int w, int h)
 	{
-		super(x, y, width, height);
+		super(x, y, w, h);
 	}
 }
