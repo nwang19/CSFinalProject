5b64bd9a8e3e5a9e41fdc8af4a2c6cbad638b53d
nwang19 <wangnicole19@gmail.com>
2017-05-24 02:43:47 -0700

    Fixed timer, simultaneous movement in x & y OK

--- CSFinalProject/src/GUI.java
+++ CSFinalProject/src/GUI.java
@@ -1,4 +1,4 @@
-iimport java.awt.*;
+import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
@@ -15,44 +15,44 @@ public class GUI extends JFrame implements ActionListener, KeyListener
     
     public GUI(Controller cont)
     {
-        timer = new Timer(250, this);
         control = cont;
         //we'll have to add gif files or something to Eclipse make the images insertable into the program
-        gameName = new JLabel("Westview Life");
+        /*gameName = new JLabel("Stressedview");
         gameName.setFont(new Font("Serif", Font.PLAIN, 50));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameName.setHorizontalAlignment(SwingConstants.CENTER);
+        gameName.setForeground(Color.white);
         playButton = new JButton("Play");
         instrButton = new JButton("Instructions"); 
         playButton.addActionListener(this);
         instrButton.addActionListener(this);
         panel = new JPanel();
-        panel.setPreferredSize(new Dimension(100, 200));
+        panel.setSize(100, 100);
+        panel.setPreferredSize(new Dimension(100, 100));
         container = getContentPane();
+        container.setBackground(Color.black);
+        panel.setBackground(Color.black);
         panel.add(playButton);
         panel.add(instrButton);
-        container.add(panel, BorderLayout.SOUTH);
-        container.add(gameName, BorderLayout.NORTH); 
-        addKeyListener(this); 
+        container.add(panel, BorderLayout.CENTER);
+        container.add(gameName, BorderLayout.NORTH);*/
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	addKeyListener(this); 
+    	timer = new Timer(250, this);
         timer.addActionListener(this);
     }
 
+    //initializes each level with components from the desired level (indicated by levelNum in parameter)
     public void init(int levelNum)
     {
         repaint();
-        
-        
-        //I'm thinking of having this method to initialize different screens?
-        //i.e. startScreen, instrScreen, level1, level2, etc.
-        //We could have a big level class (and levels extending from it) to hold each
-        //level's platforms and obstacles
     }
 
     //displays game
-   public void display()
+    public void display()
     {
         this.setSize(1000, 1000);
         this.setVisible(true);
-        repaint();
+        //repaint();
     }
 
  
@@ -77,21 +77,21 @@ public class GUI extends JFrame implements ActionListener, KeyListener
       {
     	  if (event.getKeyChar() == 'w' || event.getKeyChar() == 'W') 
           {
-              timer.start();
               control.getPlayer().setYState(Player.UP);
+              timer.start();
           }
       }
 
       //Stops the player's movement and resets player's state to still
-      public void keyReleased(KeyEvent event) 
-      {
-          //DONT FORGET TO HAVE A SEPARTE PLACE THAT STOPS THE JUMP MOTION
-    	  if(event.getKeyCode() ==  KeyEvent.VK_D || event.getKeyCode() ==  KeyEvent.VK_A)
-          {
-    		  timer.stop();
-    		  control.getPlayer().setXState(Player.STILL);
-          }
-      }
+     public void keyReleased(KeyEvent event) 
+     {
+         //DONT FORGET TO HAVE A SEPARTE PLACE THAT STOPS THE JUMP MOTION
+    	 if(event.getKeyCode() ==  KeyEvent.VK_D || event.getKeyCode() ==  KeyEvent.VK_A)
+         {
+    	  timer.stop();
+    	  control.getPlayer().setXState(Player.STILL);
+         }
+     }
  
     //public void repaint()
     
@@ -129,7 +129,7 @@ public class GUI extends JFrame implements ActionListener, KeyListener
         //g.drawRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);
         //g.fillRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);  
         g.drawImage(image, (int)control.getPlayer().getX(), (int)control.getPlayer().getY(), null);
-		System.out.println("painted a level");
+		//System.out.println("painted a level");
     }
     
     public void actionPerformed(ActionEvent event)
@@ -144,22 +144,23 @@ public class GUI extends JFrame implements ActionListener, KeyListener
         {
         	JOptionPane.showMessageDialog(this, "Use the up, left, and right arrow keys to move and to jump over obstacles" );
         }
-        else
+        else if (event.getSource() == timer)
         {
-        	Point moveLoc = control.getPlayer().getLocation();
-            if (event.getSource() == timer)
+            if (control.getPlayer().getXState() == Player.STILL && control.getPlayer().getYState() == Player.STILL)	
+            	timer.stop();
+            //else
             {
+            	Point moveLoc = control.getPlayer().getLocation();
             	if (control.getPlayer().getXState() == Player.LEFT)
                     moveLoc = new Point((int)moveLoc.getX()-10, (int)moveLoc.getY());
-                else if (control.getPlayer().getXState() == Player.RIGHT)
+            	else if (control.getPlayer().getXState() == Player.RIGHT)
                     moveLoc = new Point((int)moveLoc.getX()+10, (int)moveLoc.getY());
-            	else if (control.getPlayer().getYState() == Player.UP)
+            	if (control.getPlayer().getYState() != Player.STILL)
                     moveLoc = control.processJump(moveLoc, timer);
+            	
+            	control.processMove(control.getLevel().checkNextLoc(moveLoc, control.getLevNum(), control.getPlayer().getXState(), control.getPlayer().getYState()));
+            	updateScreen(control.getPlayer());
             }
-            control.processMove(control.getLevel().checkNextLoc(moveLoc, control.getLevNum(), control.getPlayer().getXState(), control.getPlayer().getYState()));
-            updateScreen(control.getPlayer());
         }
     }
-
-    //Use classloader when reading in images (in GuiTest samples)
 }
