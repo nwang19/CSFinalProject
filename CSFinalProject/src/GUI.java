5fd7d1b7e58d317736d5ce66e342ee4453d2af85
nwang19 <wangnicole19@gmail.com>
2017-05-24 09:59:28 -0700

    Update GUI.java

--- CSFinalProject/src/GUI.java
+++ CSFinalProject/src/GUI.java
@@ -12,12 +12,14 @@ public class GUI extends JFrame implements ActionListener, KeyListener
     private JPanel panel; 
     private JLabel gameName;
     private Timer timer;
+    private boolean startScreen;
     
     public GUI(Controller cont)
     {
         control = cont;
+        startScreen = true;
         //we'll have to add gif files or something to Eclipse make the images insertable into the program
-        /*gameName = new JLabel("Stressedview");
+        gameName = new JLabel("Stressedview");
         gameName.setFont(new Font("Serif", Font.PLAIN, 50));
         gameName.setHorizontalAlignment(SwingConstants.CENTER);
         gameName.setForeground(Color.white);
@@ -34,7 +36,7 @@ public class GUI extends JFrame implements ActionListener, KeyListener
         panel.add(playButton);
         panel.add(instrButton);
         container.add(panel, BorderLayout.CENTER);
-        container.add(gameName, BorderLayout.NORTH);*/
+        container.add(gameName, BorderLayout.NORTH);
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	addKeyListener(this); 
     	timer = new Timer(250, this);
@@ -88,7 +90,6 @@ public class GUI extends JFrame implements ActionListener, KeyListener
          //DONT FORGET TO HAVE A SEPARTE PLACE THAT STOPS THE JUMP MOTION
     	 if(event.getKeyCode() ==  KeyEvent.VK_D || event.getKeyCode() ==  KeyEvent.VK_A)
          {
-    	  timer.stop();
     	  control.getPlayer().setXState(Player.STILL);
          }
      }
@@ -97,47 +98,49 @@ public class GUI extends JFrame implements ActionListener, KeyListener
     
     public void paint(Graphics g)
     {
-        Image offImage = createImage(1000, 1000);
-        Graphics buffer = offImage.getGraphics();
-        paintOffScreen(buffer);        
-        g.drawImage(offImage, 0, 0, null);    
+    	Image offImage = createImage(1000, 1000);
+    	Graphics buffer = offImage.getGraphics();
+   		paintOffScreen(buffer);        
+   		g.drawImage(offImage, 0, 0, null);      
     }
 
     public void paintOffScreen(Graphics g)
     {
-    	ClassLoader cldr = this.getClass().getClassLoader();
-    	ImageIcon playerIcon = new ImageIcon(cldr.getResource("PlayerImg.png"));
- 		Image image = playerIcon.getImage();
-    	g.clearRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);
-        Level lev = control.getLevel();
-        //Platform plat1 = lev.getPlatforms().get(0);
-        //System.out.println("platform info: "+ plat1.getX() + " " + plat1.getY() + " " + Platform.width + " " + Platform.height);
-        for(Platform plat: lev.getPlatforms())
+    	if (startScreen == false)
         {
-        	g.setColor(Color.black);
-            g.drawRect((int)plat.getX(), (int)plat.getY(), Platform.width, Platform.height);
-            g.fillRect((int)plat.getX(), (int)plat.getY(), Platform.width, Platform.height);    
-        }
+    		ClassLoader cldr = this.getClass().getClassLoader();
+    		ImageIcon playerIcon = new ImageIcon(cldr.getResource("PlayerImg.png"));
+    		Image image = playerIcon.getImage();
+ 			g.clearRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);
+ 			Level lev = control.getLevel();
+ 			//Platform plat1 = lev.getPlatforms().get(0);
+ 			//System.out.println("platform info: "+ plat1.getX() + " " + plat1.getY() + " " + Platform.width + " " + Platform.height);
+ 			for(Platform plat: lev.getPlatforms())
+ 			{
+ 				g.setColor(Color.black);
+ 				g.drawRect((int)plat.getX(), (int)plat.getY(), Platform.width, Platform.height);
+ 				g.fillRect((int)plat.getX(), (int)plat.getY(), Platform.width, Platform.height);    
+ 			}
+ 			
+ 			for(Obstacle obs: lev.getObstacles())
+ 			{
+ 				g.setColor(Color.red);
+ 				g.drawRect((int)obs.getX(), (int)obs.getY(), Obstacle.width, Obstacle.height);
+ 				g.fillRect((int)obs.getX(), (int)obs.getY(), Obstacle.width, Obstacle.height);    
+ 			}
         
-        for(Obstacle obs: lev.getObstacles())
-        {
-        	g.setColor(Color.red);
-            g.drawRect((int)obs.getX(), (int)obs.getY(), Obstacle.width, Obstacle.height);
-            g.fillRect((int)obs.getX(), (int)obs.getY(), Obstacle.width, Obstacle.height);    
+ 			g.drawImage(image, (int)control.getPlayer().getX(), (int)control.getPlayer().getY(), null);
         }
-        
-        //g.drawRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);
-        //g.fillRect((int)control.getPlayer().getX(), (int)control.getPlayer().getY(), Controller.pWidth, Controller.pHeight);  
-        g.drawImage(image, (int)control.getPlayer().getX(), (int)control.getPlayer().getY(), null);
-		//System.out.println("painted a level");
     }
     
     public void actionPerformed(ActionEvent event)
     {
     	if (event.getSource() == playButton)
         {
-            container.removeAll();
-            init(1); //should display game at level specified in parameter
+    		container.removeAll();
+            startScreen = false;
+            revalidate();
+    		init(1); //should display game at level specified in parameter
            
         }
         else if (event.getSource() == instrButton)
