4977c28a8e029f9e7b3d071dbfdb47767bdd6633
nwang19 <wangnicole19@gmail.com>
2017-06-01 09:20:47 -0700

    Update Platform.java

--- CSFinalProject/src/Platform.java
+++ CSFinalProject/src/Platform.java
@@ -1,10 +1,7 @@
 import java.awt.Rectangle;
 
 public class Platform extends Rectangle
-{
-	static int width = 300;
-	static int height = 20;
-	
+{	
 	public Platform(int x, int y, int w, int h)
 	{
 		super(x, y, w, h);
