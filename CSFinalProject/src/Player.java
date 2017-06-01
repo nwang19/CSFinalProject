9f4a7a86b5a9e4122a52a1e5e6a0c02a3e05a093
nwang19 <wangnicole19@gmail.com>
2017-06-01 09:21:25 -0700

    Update Player.java

--- CSFinalProject/src/Player.java
+++ CSFinalProject/src/Player.java
@@ -2,9 +2,8 @@ import java.awt.Rectangle;
 
 public class Player extends Rectangle
 {
-	//Job: stores lives, gets location to move to next, and updates its record of its position.
+	//Job: gets location to move to next, and updates its record of player's position.
 
-	//private int lives; 
 	private int xState;
 	private int yState;
 	private boolean onPlat;
@@ -16,7 +15,6 @@ public class Player extends Rectangle
 	
 	public Player()
 	{
-		//lives = 3;
 		xState = STILL;
 		yState = STILL;
 	}
@@ -24,48 +22,44 @@ public class Player extends Rectangle
 	public Player(int x, int y, int width, int height)
 	{
 		super(x, y, width, height);
-		//lives = 3;
 		xState = STILL;
 		yState = STILL;
+		onPlat = true;
 	}
-
+	
+	//Changes direction of player's movement in the x-direction to Player.STILL, Player.LEFT, or Player.RIGHT
 	public void setXState(int status)
 	{
 		xState = status;
 	}
 	
+	//Returns x-direction of player's movement
 	public int getXState()
 	{
 		return xState;
 	}
 	
+	//Returns y-direction of player's movement
 	public int getYState()
 	{
 		return yState;
 	}
 	
+	//Changes direction of player's movement in the y-direction to Player.STILL, Player.UP, or Player.DOWN
 	public void setYState(int y)
 	{
 		yState = y;
 	}
 	
+	//Returns true if player is on a platform, otherwise returns false
 	public boolean isOnPlat()
 	{
 		return onPlat;
 	}
 	
+	//Updates record of whether or not the player is on a platform
 	public void setPlatState(boolean state)
 	{
 		onPlat = state;
 	}
-	/*public int getLives()
-	{
-		return lives;
-	}
-	
-	public void setLives(int numLives)
-	{
-		lives = numLives;
-	}*/
-
 }
