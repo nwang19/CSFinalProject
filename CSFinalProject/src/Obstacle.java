397d1eac851e744c5fb378a41ba08e87b887e6d1
nwang19 <wangnicole19@gmail.com>
2017-06-01 09:21:06 -0700

    Update Obstacle.java

--- CSFinalProject/src/Obstacle.java
+++ CSFinalProject/src/Obstacle.java
@@ -1,28 +1,13 @@
-import java.awt.Image;
-import java.awt.Point;
 import java.awt.Rectangle; 
 
 public class Obstacle extends Rectangle
 {
-	private Point myPoint;
-	static int width = 30;
-	static int height = 30;
+	static final int WIDTH = 30;
+	static final int HEIGHT = 30;
 	
 	public Obstacle(int x, int y)
 	{
-		super(x, y, width, height);
-		myPoint = new Point(x, y-(height/2));
-	}
-	
-	public Point getPoint()
-	{
-		return myPoint;
-	}
-	
-	public void setPoint(Point p)
-	{
-		myPoint = p;
-		setLocation((int)p.getX(), (int)p.getY() + (height/2));
+		super(x, y, WIDTH, HEIGHT);
 	}
 	
 }
