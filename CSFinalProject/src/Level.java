97ed40b6cb0e580f863e7b381d45a6aff9d82e07
RiversMountains <vuppalad@gmail.com>
2017-05-31 23:00:15 -0700

    Update Level.java

--- CSFinalProject/src/Level.java
+++ CSFinalProject/src/Level.java
@@ -3,7 +3,7 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class Level {
-	static final Point start = new Point(25, 600);
+	private Point start;
 	private int screenWidth, screenHeight;
 	private Point end;
 	private ArrayList<Obstacle> obstacles;
@@ -19,41 +19,53 @@ public class Level {
 		baseline = 700;
 		if (level == 1)
 		{
+			start = new Point(0, 200);
 			end = new Point(1000, 690);
 			obstacles = new ArrayList<Obstacle>();
-			for (int x = 150; x <= 350; x += 150)
-			//	obstacles.add(new Seagull(x, baseline - Obstacle.height - 65));
-			obstacles.add(new ToxicAP(500, baseline-7));
-			obstacles.add(new ToxicAP(500-Obstacle.width, baseline-7));
-			obstacles.add(new ToxicAP(500-(2*Obstacle.width), baseline-7));
-			obstacles.add(new ToxicAP(700, baseline-7));
-			obstacles.add(new ToxicAP(700-Obstacle.width, baseline-7));
-			obstacles.add(new ToxicAP(700-(2*Obstacle.width), baseline-7));
-			obstacles.add(new Book(900, baseline-15));
+			obstacles.add(new Seagull(180, baseline - 180));
+			obstacles.add(new Book(400, baseline-15));
+			obstacles.add(new Book(750, baseline-15));
+			obstacles.add(new ToxicAP(200, baseline-408));
+			obstacles.add(new ToxicAP(200-Obstacle.width, baseline-408));
 			platforms = new ArrayList<Platform>();
 			for (int x = 0; x < (int)end.getX(); x += Platform.width)
-				platforms.add(new Platform(x, baseline, Obstacle.width, Obstacle.height));
-			//platforms.add(new Platform(70, baseline - 50, 300, 20));
-			platforms.add(new MovingObject(50, baseline - 50, 20, 300, Player.RIGHT, 25, 300));
+				platforms.add(new Platform(x, baseline, 300, 20));
+			platforms.add(new Platform(0, baseline - 400, 300, 20));
+			platforms.add(new Platform(350, baseline - 350, 250, 20));
+			platforms.add(new Platform(550, baseline - 200, 120, 20));
+			platforms.add(new Platform(180, baseline - 125, 180, 20));
+			platforms.add(new MovingObject(70, baseline - 50, 300, 20, Player.LEFT, 25, 500));
 		}
 		else if (level == 2)
 		{
+			start = new Point(0, 200);
 			end = new Point(1000, 690);
 			obstacles = new ArrayList<Obstacle>();
-			obstacles.add(new ToxicAP(200, baseline-7));
-			obstacles.add(new ToxicAP(200-Obstacle.width, baseline-7));
-			obstacles.add(new ToxicAP(200-(2*Obstacle.width), baseline-7));
-			obstacles.add(new Seagull(300, baseline-80));
-			obstacles.add(new Seagull(500, baseline-80));
-			obstacles.add(new Book(700, baseline-30));
-			obstacles.add(new Seagull(900, baseline-60)); 
+			obstacles.add(new ToxicAP(600, baseline-7));
+			obstacles.add(new ToxicAP(600-Obstacle.width, baseline-7));
+			obstacles.add(new ToxicAP(220, baseline-210));
+			obstacles.add(new ToxicAP(220-Obstacle.width, baseline-210));
+			obstacles.add(new Book(200, baseline-30));
+			obstacles.add(new Book(350, baseline-30));
+			obstacles.add(new Book(760, baseline-30));
+			obstacles.add(new Seagull(150, baseline-250));
+			obstacles.add(new Seagull(580, baseline-470));
+			obstacles.add(new Seagull(630, baseline-470));
+			obstacles.add(new Seagull(770, baseline-326)); 
 			
 			platforms = new ArrayList<Platform>();
 			for (int x = 0; x < (int)end.getX(); x += Platform.width)
-				platforms.add(new Platform(x, baseline, 10, 10));
+				platforms.add(new Platform(x, baseline,300, 20));
+			platforms.add(new Platform(0, baseline - 400, 150, 20));
+			platforms.add(new Platform(180, baseline - 200, 100, 20));
+			platforms.add(new Platform(250, baseline - 350, 200, 20));
+			//platforms.add(new MovingObject(250, baseline - 350, 250, 20, Player.LEFT, 25, 500));
+			platforms.add(new Platform(550, baseline - 420, 120, 20));
+			platforms.add(new Platform(700, baseline - 280, 100, 20));
 		}
 		else if (level == 3)
 		{
+			start = new Point(0, 200);
 			obstacles = new ArrayList<Obstacle>();
 			obstacles.add(new ToxicAP(300, baseline-7));
 			obstacles.add(new ToxicAP(300-Obstacle.width, baseline-7));
@@ -75,6 +87,7 @@ public class Level {
 		}
 		else if (level == 4)
 		{
+			start = new Point(0, 200);
 			end = new Point(1000, 690);
 			obstacles = new ArrayList<Obstacle>();
 			obstacles.add(new Book(200, baseline-30));
@@ -110,6 +123,10 @@ public class Level {
 		return end;
 	}
 	
+	public Point getStart()
+	{
+		return start;
+	}
 	
 	public int getLevelNum()
 	{
@@ -171,7 +188,7 @@ public class Level {
 			for (Obstacle obst : obstacles)
 			{
 				if (obst != null && obst.intersects(next))
-					return Level.start;
+					return start;
 			}
 			for (Platform plat : platforms)
 			{
